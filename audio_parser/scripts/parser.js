1b138c04ba2d70158897978ef3d230b58840343a
Randy Komforty <randykomforty@gmail.com>
2025-06-20 00:45:23 -0700

    update audio_parser

--- audio_parser/scripts/parser.js
+++ audio_parser/scripts/parser.js
@@ -2,36 +2,92 @@ import morphemeList from "./silvanus.js";
 console.log(morphemeList);
 let myAudio = document.querySelector("audio");
 let i = 0;
-let j = 0;
-let k = 0;
+let ij = 0;
+let ijk = 0;
+let ijkl = 0;
 for (i = 0; i < morphemeList.length; i++) {
 	
-	// li for each line
+	console.log("<li> " + i);
+	
+	// #interlinear li
 	let newLine = document.createElement("li");
 	newLine.classList.add("line");
 	document.querySelector("#interlinear").appendChild(newLine);
 	
-	// ul for each sub-line
+	console.log("\t<ul>");
+	
+	// #interlinear li.line ul.sub-line
 	let newSubLine = document.createElement("ul");
 	newSubLine.classList.add("sub-line");
 	document.getElementsByClassName("line")[i].appendChild(newSubLine);
 	
-	// li for each whole-morpheme
+	console.log("\t\t<li class=\"whole-morpheme\">");
+	
+	// #interlinear li.line ul.sub-line li[0]
 	let newWholeMorpheme = document.createElement("li");
 	newWholeMorpheme.classList.add("whole-morpheme");
 	document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].appendChild(newWholeMorpheme);
 	
-	// li for each whole-gloss
-	let newWholeGloss = document.createElement("li");
-	newWholeGloss.classList.add("whole-gloss");
-	document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].appendChild(newWholeGloss);
-	
-	for (j = 0; j < morphemeList[i][0].length; j++) {
-		console.log(morphemeList[i][0].length);
+	for (ij = 0; ij < morphemeList[i][0].length; ij++) {
+		
+		console.log("\t\t\t<div class=\"group\">" + ij);
 		let newGroup = document.createElement("div");
 		newGroup.classList.add("group");
 		document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].appendChild(newGroup);
+		
+		for (ijk = 0; ijk < morphemeList[i][0][ij].length; ijk++) {
+			
+			console.log("\t\t\t\t<div class=\"morpheme\">" + ijk);
+			let newMorpheme = document.createElement("a");
+			newMorpheme.classList.add("morpheme");
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].appendChild(newMorpheme);
+			
+			
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].setAttribute("data-timestamp", morphemeList[i][0][ij][ijk][0]);
+			
+			if (morphemeList[i][0][ij][ijk][3] != "punc") {
+				
+				document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].setAttribute("href", "#");
+				
+			}
+			
+			if (morphemeList[i][0][ij][ijk][3] == "punc") {
+				
+				document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].classList.add("punc");
+				
+			}
+			
+			/*
+			let newTime = document.createElement("span");
+			newTime.classList.add("time");
+			newTime.textContent = morphemeList[i][0][ij][ijk][0];
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].appendChild(newTime);
+			*/
+			
+			let newPos = document.createElement("span");
+			newPos.classList.add("pos");
+			newPos.textContent = morphemeList[i][0][ij][ijk][3];
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].appendChild(newPos);
+
+			let newNorm = document.createElement("span");
+			newNorm.classList.add("norm");
+			newNorm.textContent = morphemeList[i][0][ij][ijk][1];
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].appendChild(newNorm);
+			
+			let newGloss = document.createElement("span");
+			newGloss.classList.add("gloss");
+			newGloss.textContent = morphemeList[i][0][ij][ijk][2];
+			document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].getElementsByClassName("whole-morpheme")[0].getElementsByClassName("group")[ij].getElementsByClassName("morpheme")[ijk].appendChild(newGloss);
+		}
 	}
+	
+	console.log("\t\t<li class=\"whole-gloss\">");
+	// #interlinear li.line ul.sub-line li[1]
+	let newWholeGloss = document.createElement("li");
+	newWholeGloss.classList.add("whole-gloss");
+	newWholeGloss.textContent = morphemeList[i][1];
+	document.getElementsByClassName("line")[i].getElementsByClassName("sub-line")[0].appendChild(newWholeGloss);
+	
 }
 const timestamps = document.getElementsByClassName("morpheme");
 myAudio.addEventListener("timeupdate", () => {
